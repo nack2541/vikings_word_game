a6d6c4c0f84f082e8575254bf1e1fba13c57de51
tanakorn <nacktanakorn41@gmail.com>
2020-04-05 14:14:45 +0700

    upload test card game project

---
+++ TestCardGame/Assets/Script/Scrambled Scripts/CharObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharObject : MonoBehaviour
+{
+    public char charactor;
+    public Text text;
+    public RectTransform rectTransform;
+    public int index;
+
+    [Header("Appearence")]
+    public Color normalColor;
+    public Color selectedColor;
+    public CharObject init(char c)
+    {
+        c=charactor;
+        text.text=c.ToString();
+        gameObject.SetActive(true);
+        return this;
+    }
+
+}
